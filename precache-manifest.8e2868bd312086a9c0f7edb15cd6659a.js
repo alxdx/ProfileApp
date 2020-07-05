e17c1c0892ee1acd027f194176fa76060fbae4e8
alxdx <ustongs09@gmail.com>
2020-07-04 23:03:30 -0500

    Updates

---
+++ precache-manifest.8e2868bd312086a9c0f7edb15cd6659a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fbf0ce44a4d8c427b44f22e83e2882e",
+    "url": "/ProfileApp/index.html"
+  },
+  {
+    "revision": "28d4e7b896472378bfae",
+    "url": "/ProfileApp/static/css/main.39a980c6.chunk.css"
+  },
+  {
+    "revision": "d73ea675c45b43a980ec",
+    "url": "/ProfileApp/static/js/2.5c20e2d1.chunk.js"
+  },
+  {
+    "revision": "3143274d7d26932c98894a52e75c487a",
+    "url": "/ProfileApp/static/js/2.5c20e2d1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "28d4e7b896472378bfae",
+    "url": "/ProfileApp/static/js/main.fdfb089a.chunk.js"
+  },
+  {
+    "revision": "b929b924b9dbb6b6e5f2",
+    "url": "/ProfileApp/static/js/runtime-main.09410f78.js"
+  },
+  {
+    "revision": "b70d733d8048e5388ee26373df95709e",
+    "url": "/ProfileApp/static/media/perfil.b70d733d.jpg"
+  }
+]);
